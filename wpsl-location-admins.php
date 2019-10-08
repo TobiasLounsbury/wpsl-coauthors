c58ecd68a5b3166060f27608319e92a17d6fc97b
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2019-10-07 17:51:48 -0700

    Added empty plugin

---
+++ wpsl-location-admins.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package   WPSL_Location_Admins
+ * @author    Tobias Lounsbury <TobiasLounsbury@gmail.com>
+ * @license   GPL-2.0+
+ * @link      https://github.com/TobiasLounsbury/wpsl-location-admins
+ * @copyright 2019 Tobias Lounsbury
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Store Locator: Custom Location Admins
+ * Plugin URI:        https://github.com/TobiasLounsbury/wpsl-location-admins
+ * Description:       Allows the metadata for individual locations to be edited by configured users
+ * Version:           1.0.0
+ * Author:            Tobias Lounsbury
+ * Author URI:        http://TobiasLounsbury.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/TobiasLounsbury/wpsl-location-admins
+ */
+
